6cf159e1137b0f78ba9abbfb187d78e77daebd5c
slyre1337 <59583684+slyre1337@users.noreply.github.com>
2020-07-28 22:32:56 +0200

    Update checkvpnport.sh

--- run/nobody/checkvpnport.sh
+++ run/nobody/checkvpnport.sh
@@ -13,3 +13,16 @@ if [[ "${APPLICATION}" != "sabnzbd" ]] && [[ "${APPLICATION}" != "privoxy" ]] &&
 	VPN_INCOMING_PORT=$(<"${vpn_port}")
 
 fi
+
+if [[ "${VPN_PROV}" == "custom" ]]; then
+
+	vpn_port="/tmp/getvpnport"
+
+	while [ ! -f "${vpn_port}" ]
+	do
+		sleep 1s
+	done
+
+	VPN_INCOMING_PORT=$(<"${vpn_port}")
+
+fi
